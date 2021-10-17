7d61de2c88acba55a186514912b2fbbbf6aa8ab9
Gordon Byers <gordon.byers@microsoft.com>
2021-10-17 20:54:20 +0100

    Update helper-test-agic-options-available.spec.js

--- .github/workflows_dep/playwright/helper-test-agic-options-available.spec.js
+++ .github/workflows_dep/playwright/helper-test-agic-options-available.spec.js
@@ -10,23 +10,17 @@ test('test', async ({ page }) => {
   await page.waitForSelector('.ms-Stack:nth-child(4) > .ms-DocumentCard:nth-child(1) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
   await page.click('.ms-Stack:nth-child(4) > .ms-DocumentCard:nth-child(1) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
 
-  await page.waitForSelector('.ms-Stack > div > .itemContainer-177 > div > .ms-Stack')
-  await page.click('.ms-Stack > div > .itemContainer-177 > div > .ms-Stack')
-
   await page.waitForSelector('#Pivot9-Tab2 > .ms-Button-flexContainer > span > .ms-Pivot-linkContent > .ms-Pivot-text')
   await page.click('#Pivot9-Tab2 > .ms-Button-flexContainer > span > .ms-Pivot-linkContent > .ms-Pivot-text')
 
   await page.waitForSelector('#ChoiceGroupLabel63-appgw')
   await page.click('#ChoiceGroupLabel63-appgw')
 
-  await page.waitForSelector('.ms-Stack > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text > .css-114')
-  await page.click('.ms-Stack > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text > .css-114')
+  await page.waitForSelector('.ms-Stack:nth-child(2) > .ms-DocumentCard:nth-child(3) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
+  await page.click('.ms-Stack:nth-child(2) > .ms-DocumentCard:nth-child(3) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
 
-  await page.waitForSelector('#Toggle0')
-  await page.click('#Toggle0')
-
-  await page.waitForSelector('.ms-DocumentCard:nth-child(2) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
-  await page.click('.ms-DocumentCard:nth-child(2) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
+  await page.waitForSelector('.ms-Stack:nth-child(4) > .ms-DocumentCard:nth-child(3) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
+  await page.click('.ms-Stack:nth-child(4) > .ms-DocumentCard:nth-child(3) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
 
   await page.waitForSelector('#ChoiceGroupLabel63-none')
   await page.click('#ChoiceGroupLabel63-none')
@@ -34,9 +28,11 @@ test('test', async ({ page }) => {
   await page.waitForSelector('#ChoiceGroupLabel63-appgw')
   await page.click('#ChoiceGroupLabel63-appgw')
 
-  await page.waitForSelector('#ChoiceGroupLabel108-Detection')
-  await page.click('#ChoiceGroupLabel108-Detection')
+  await page.waitForSelector('#ChoiceGroupLabel92-Detection')
+  await page.click('#ChoiceGroupLabel92-Detection')
 
-  await page.waitForSelector('#ChoiceGroupLabel108-Prevention')
-  await page.click('#ChoiceGroupLabel108-Prevention')
+  await page.waitForSelector('#ChoiceGroupLabel92-Prevention')
+  await page.click('#ChoiceGroupLabel92-Prevention')
+
+  await page.screenshot({ path: 'screenshot_1.png', fullPage: true })
 });
