e89d0c71126662a22831281f1a73dcd7671914d4
Gordon Byers <gordon.byers@microsoft.com>
2021-10-17 21:07:44 +0100

    Update helper-test-agic-options-available.spec.js

--- .github/workflows_dep/playwright/helper-test-agic-options-available.spec.js
+++ .github/workflows_dep/playwright/helper-test-agic-options-available.spec.js
@@ -15,6 +15,8 @@ test('test', async ({ page }) => {
 
   await page.waitForSelector('#ChoiceGroupLabel63-appgw')
   await page.click('#ChoiceGroupLabel63-appgw')
+  
+  await page.screenshot({ path: 'screengrabs/simple-simple-addons.png', fullPage: true })
 
   await page.waitForSelector('.ms-Stack:nth-child(2) > .ms-DocumentCard:nth-child(3) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
   await page.click('.ms-Stack:nth-child(2) > .ms-DocumentCard:nth-child(3) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
@@ -34,5 +36,22 @@ test('test', async ({ page }) => {
   await page.waitForSelector('#ChoiceGroupLabel92-Prevention')
   await page.click('#ChoiceGroupLabel92-Prevention')
 
-  await page.screenshot({ path: 'screengrabs/screenshot_1.png', fullPage: true })
+  await page.screenshot({ path: 'screengrabs/managed-private-addons.png', fullPage: true })
+  
+  //testing to see if prevention/detection are there for simple-simple
+  await page.waitForSelector('.ms-Stack:nth-child(2) > .ms-DocumentCard:nth-child(1) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
+  await page.click('.ms-Stack:nth-child(2) > .ms-DocumentCard:nth-child(1) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
+
+  await page.waitForSelector('.ms-Stack:nth-child(4) > .ms-DocumentCard:nth-child(1) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
+  await page.click('.ms-Stack:nth-child(4) > .ms-DocumentCard:nth-child(1) > .ms-DocumentCardDetails > .ms-Checkbox > .ms-Checkbox-label > .ms-Checkbox-text')
+
+  await page.waitForSelector('#Pivot9-Tab2 > .ms-Button-flexContainer > span > .ms-Pivot-linkContent > .ms-Pivot-text')
+  await page.click('#Pivot9-Tab2 > .ms-Button-flexContainer > span > .ms-Pivot-linkContent > .ms-Pivot-text')
+
+  await page.waitForSelector('#ChoiceGroupLabel63-appgw')
+  await page.click('#ChoiceGroupLabel63-appgw')
+  
+  //await page.waitForSelector('#ChoiceGroupLabel92-Prevention')
+  const visible = await page.isVisible('#ChoiceGroupLabel92-Prevention');
+  expect(visible).toBeTruthy();
 });
