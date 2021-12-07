3a6a460ebcac2b2a6c223cab6186d6910af74243
Gordon Byers <gordon.byers@microsoft.com>
2021-12-07 08:00:13 +0000

    changed default - fixing test.

--- helper/.playwrighttests/helper-test-disablelocalaccounts-checkbox-clicommand.spec.js
+++ helper/.playwrighttests/helper-test-disablelocalaccounts-checkbox-clicommand.spec.js
@@ -7,39 +7,37 @@ expect.extend(matchers);
 const chk = '+ label > .ms-Checkbox-checkbox > .ms-Checkbox-checkmark' //dom hack to get to the checkbox
 
 test('networkpolicy-test-defaul-is-azure', async ({ page }) => {
- 
+
   await page.goto('http://localhost:3000/Aks-Construction');
-  
+
   //Is the CLI textarea there and visible?
   const cliboxvis = await page.isVisible('[data-testid="deploy-deploycmd"]');
   expect(cliboxvis).toBeTruthy();
-  
-  //It shouldn't yet contain the bastion text
+
+  //Check settings is there for default config
   await page.waitForSelector('[data-testid="deploy-deploycmd"]')
   const clitextbox = await page.$('[data-testid="deploy-deploycmd"]')
   await expect(clitextbox).toBeVisible()
-  await expect(clitextbox).not.toContainText('aksDisableLocalAccounts');
-
-  //But i am expecting enable_aad to be there
+  await expect(clitextbox).toContainText('aksDisableLocalAccounts')
   await expect(clitextbox).toContainText('enable_aad=true')
 
-  // Click the 2nd Tab in the portal Navigation Pivot (cluster details) 
+  // Click the 2nd Tab in the portal Navigation Pivot (cluster details)
   await page.click('[data-testid="portalnav-Pivot"] > button:nth-child(2)')
 
   //Inspect the checkbox
   await page.waitForSelector('[data-testid="cluster-localaccounts-Checkbox"]')
   const checkbox = await page.$('[data-testid="cluster-localaccounts-Checkbox"]')
-  await expect(checkbox).not.toBeChecked();
+  await expect(checkbox).toBeChecked();
   await expect(checkbox).toBeVisible();
 
-  //Enable Checkbox
+  //Disable the Checkbox
   await page.click('[data-testid="cluster-localaccounts-Checkbox"]' + chk)
-  await expect(checkbox).toBeChecked();
- 
+  await expect(checkbox).not.toBeChecked();
+
   //Go back to the deploy tab.
   await page.click('[data-testid="portalnav-Pivot"] > button:nth-child(1)')
 
-  //The setting for bastion should be there now
+  //The setting should have disappeared now
   const clitextboxrevisted = await page.$('[data-testid="deploy-deploycmd"]')
-  await expect(clitextboxrevisted).toContainText('aksDisableLocalAccounts');
+  await expect(clitextboxrevisted).not.toContainText('aksDisableLocalAccounts');
 });
