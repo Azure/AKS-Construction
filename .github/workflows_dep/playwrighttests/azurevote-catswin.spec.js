2ec3cf25d1e8fb5ce52169a36336bc47d17b7c5e
Gordon Byers <gordon.byers@microsoft.com>
2021-12-14 13:09:23 +0000

    adding playwright test for voteapp

---
+++ .github/workflows_dep/playwrighttests/azurevote-catswin.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require('@playwright/test');
+const { matchers } = require('playwright-expect');
+
+test('azurevote-catswin', async ({ page }) => {
+  console.log('http://' + process.env.APPIP);
+  await page.goto('http://' + process.env.APPIP)
+
+  //Click Vote for Cats x3
+  await page.waitForSelector('#container > #form > center > #form > .button1')
+  await page.click('#container > #form > center > #form > .button1')
+  await navigationPromise
+  await page.waitForSelector('#container > #form > center > #form > .button1')
+  await page.click('#container > #form > center > #form > .button1')
+  await navigationPromise
+  await page.waitForSelector('#container > #form > center > #form > .button1')
+  await page.click('#container > #form > center > #form > .button1')
+  await navigationPromise
+
+  //Click Vote for Dogs x1
+  await page.waitForSelector('#container > #form > center > #form > .button2')
+  await page.click('#container > #form > center > #form > .button2')
+  await navigationPromise
+
+  await page.waitForSelector('#results')
+  await expect('#results').not.toContainText('Cats - 3 | Dogs - 1');
+
+});
