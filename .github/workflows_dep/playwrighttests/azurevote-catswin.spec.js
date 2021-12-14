9a62eed09f283904b93a6714850f4670b8a60533
Gordon Byers <gordon.byers@microsoft.com>
2021-12-14 14:22:28 +0000

    playwright test

--- .github/workflows_dep/playwrighttests/azurevote-catswin.spec.js
+++ .github/workflows_dep/playwrighttests/azurevote-catswin.spec.js
@@ -5,21 +5,28 @@ test('azurevote-catswin', async ({ page }) => {
   console.log('http://' + process.env.APPIP);
   await page.goto('http://' + process.env.APPIP)
 
+  //Click reset button
+  await page.waitForSelector('#container > #form > center > #form > .button3')
+  await page.click('#container > #form > center > #form > .button3')
+
   //Click Vote for Cats x3
   i=0
-  while (i < 4) {
+  while (i < 3) {
     await page.waitForSelector('#container > #form > center > #form > .button1')
     await page.click('#container > #form > center > #form > .button1')
-    //await navigationPromise
     i++;
   }
 
   //Click Vote for Dogs x1
   await page.waitForSelector('#container > #form > center > #form > .button2')
   await page.click('#container > #form > center > #form > .button2')
-  //await navigationPromise
 
-  await page.waitForSelector('#results')
-  await expect('#results').not.toContainText('Cats - 3 | Dogs - 1');
+  //Check the results div
+  const results = await page.$('#results')
+  await expect('#results').toBeVisible
+  console.log(await results.innerText());
+  if (await results.innerText() != 'Cats - 3 | Dogs - 1') {
+    throw "Incorrect result"
+  }
 
 });
