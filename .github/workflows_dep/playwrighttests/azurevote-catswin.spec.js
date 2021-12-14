f55ce814defec04924d500d42c0659c2ed19ebb7
Gordon Byers <gordon.byers@microsoft.com>
2021-12-14 13:29:05 +0000

    loop

--- .github/workflows_dep/playwrighttests/azurevote-catswin.spec.js
+++ .github/workflows_dep/playwrighttests/azurevote-catswin.spec.js
@@ -6,15 +6,12 @@ test('azurevote-catswin', async ({ page }) => {
   await page.goto('http://' + process.env.APPIP)
 
   //Click Vote for Cats x3
-  await page.waitForSelector('#container > #form > center > #form > .button1')
-  await page.click('#container > #form > center > #form > .button1')
-  await navigationPromise
-  await page.waitForSelector('#container > #form > center > #form > .button1')
-  await page.click('#container > #form > center > #form > .button1')
-  await navigationPromise
-  await page.waitForSelector('#container > #form > center > #form > .button1')
-  await page.click('#container > #form > center > #form > .button1')
-  await navigationPromise
+  while (i < 4) {
+    await page.waitForSelector('#container > #form > center > #form > .button1')
+    await page.click('#container > #form > center > #form > .button1')
+    await navigationPromise
+    i++;
+  }
 
   //Click Vote for Dogs x1
   await page.waitForSelector('#container > #form > center > #form > .button2')
