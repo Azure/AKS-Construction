bc2b568c222ba0182cef4d5d4d56f322985f2b7b
Gordonby <gordon.byers@microsoft.com>
2022-05-09 14:08:09 +0100

    bumping AzureVote Helm chart version
    Signed-off-by: Gordonby <gordon.byers@microsoft.com>

--- postdeploy/scripts/install-vote-apps.sh
+++ postdeploy/scripts/install-vote-apps.sh
@@ -11,7 +11,7 @@ respcode=$(curl -o /dev/null -s -w "%{http_code}\n" $SIMPLEIP)
 echo "SimpleVote ($SIMPLEIP): HTTP $respcode"
 
 echo "Installing Ingress-Vote, Public IP on AGIC"
-helm upgrade --namespace agic-public --create-namespace --install avote-public https://github.com/Gordonby/minihelm/raw/main/samples/AzureVote-0.9.5.tgz --set ingress.enabled=true,ingress.appGwPrivateIp=false,front.service.azureLbInternal=false,front.service.type="ClusterIP"
+helm upgrade --namespace agic-public --create-namespace --install avote-public https://github.com/Gordonby/minihelm/raw/main/samples/AzureVote-0.9.8.tgz --set ingress.enabled=true,ingress.appGwPrivateIp=false,front.service.azureLbInternal=false,front.service.type="ClusterIP"
 kubectl get svc -n agic-public
 INGRESSIP=$(kubectl get ing avote-public -n agic-public  -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 respcodeing=$(curl -o /dev/null -s -w "%{http_code}\n" $INGRESSIP)
