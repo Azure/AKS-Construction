b445a969bd57b3a1ec43cd3791edd60fb6c605fd
Gordon Byers <gordon.byers@microsoft.com>
2022-06-07 21:31:09 +0000

    Fixed icon clicked behaviour

--- helper/src/components/common.js
+++ helper/src/components/common.js
@@ -23,6 +23,7 @@ export const adv_stackstyle = { root: { border: "1px solid", margin: "10px 0", p
 
 export function CodeBlock({deploycmd, testId, lang, filename}) {
     const [ copied, setCopied ] = useState(false)
+    const [ downloaded, setDownloaded ] = useState(false)
 
     function copyIt() {
         navigator.clipboard.writeText(deploycmd)
@@ -32,21 +33,31 @@ export function CodeBlock({deploycmd, testId, lang, filename}) {
 
     function downloadIt(){
         function dataUrl(data) {return "data:x-application/text," + escape(deploycmd);}
-        window.open(dataUrl());
+        setDownloaded(true)
+        window.open(dataUrl())
+        setTimeout(() => setDownloaded(false), 1000)
     }
 
     return [
         <div key="code-header" className="codeHeader" >
             <span className="language">{lang}</span>
             <CommandBarButton
-              //disabled={filename.length()>0}
+              disabled={downloaded}
               className="action position-relative"
-              iconProps={{ iconName: copied? 'Completed' : 'Save'}}
+              iconProps={{ iconName: downloaded ? 'Completed' : 'Save'}}
               styles={{icon: {color: '#171717'}}}
               text="Save"
               primaryActionButtonProps={{download: filename}}
-              onClick={downloadIt}/>
-            <CommandBarButton disabled={copied} className="action position-relative" iconProps={{ iconName: copied? 'Completed' : 'Copy'}} styles={{icon: {color: '#171717'}}} text="Copy" onClick={copyIt}/>
+              onClick={downloadIt}
+            />
+            <CommandBarButton
+              disabled={copied}
+              className="action position-relative"
+              iconProps={{ iconName: copied ? 'Completed' : 'Copy'}}
+              styles={{icon: {color: '#171717'}}}
+              text="Copy"
+              onClick={copyIt}
+            />
         </div>,
 
         <pre key="code-pre" className="has-inner-focus">
