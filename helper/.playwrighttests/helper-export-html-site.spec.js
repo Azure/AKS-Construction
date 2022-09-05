a9a56b67fac1c0efe2926b961c03d910115a91f0
Gordon Byers <gordon.byers@microsoft.com>
2022-09-05 19:54:31 +0000

    adding prodtest

--- helper/.playwrighttests/helper-export-html-site.spec.js
+++ helper/.playwrighttests/helper-export-html-site.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const fs = require('fs');
 
-test('test', async ({ page }) => {
+test('localsite', async ({ page }) => {
 
   await page.goto('http://localhost:3000/AKS-Construction');
 
@@ -14,5 +14,21 @@ test('test', async ({ page }) => {
 
   console.log(pageHtml);
   fs.writeFileSync('localsite.html', pageHtml);
+
+});
+
+test('prodsite', async ({ page }) => {
+
+  await page.goto('https://azure.github.io/AKS-Construction/');
+
+  //Wait for the stack
+  const stackops='[data-testid="stackops"]';
+  await page.waitForSelector(stackops)
+
+  //Save the contents of the az cmd box to file
+  const pageHtml = await page.content();
+
+  console.log(pageHtml);
+  fs.writeFileSync('prodsite.html', pageHtml);
 
 });
