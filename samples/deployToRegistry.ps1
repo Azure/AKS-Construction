337c92eac4e45bfc5a8fea8ff254bebb83fbe7dc
Gordon Byers <gordon.byers@microsoft.com>
2022-09-07 15:28:09 +0100

    abstracted

--- samples/deployToRegistry.ps1
+++ samples/deployToRegistry.ps1
@@ -1,8 +1,13 @@
 # Shows how to deploy the AKS Construction code to an Azure Container Registry
 # Based on the 0.8.9 release of AKSC
 
-$ACRNAME=az acr show -g iac -n gobyers --query loginServer -o tsv
-echo $ACRNAME
+$RG=yourResourceGroup
+$ACRNAME=yourAcrName
 
-az bicep publish -f ../bicep/main.bicep -t br:$ACRNAME/bicep/modules/aksc:0.8.9
-az bicep publish -f ../bicep/main.bicep -t br:$ACRNAME/bicep/modules/aksc:latest
+# Get the ACR Endpoint
+$ACRENDPOINT=az acr show -g $RG -n $ACRNAME --query loginServer -o tsv
+echo $ACRENDPOINT
+
+#Publish the AKS Bicep
+az bicep publish -f ../bicep/main.bicep -t br:$ACRENDPOINT/bicep/modules/aksc:0.8.9
+az bicep publish -f ../bicep/main.bicep -t br:$ACRENDPOINT/bicep/modules/aksc:latest
