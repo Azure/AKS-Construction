b0bcbc4d2a9973ab3fd79c14f69252c19075dcb7
samaea <32162601+samaea@users.noreply.github.com>
2023-01-06 14:06:03 +0000

    Postdeploy fix.

--- postdeploy/scripts/postdeploy.sh
+++ postdeploy/scripts/postdeploy.sh
@@ -405,13 +405,12 @@ if [ "$denydefaultNetworkPolicy" ]; then
 fi
 
 if [ "$containerLogsV2" ]; then
-    replace="\# containerlog_schema_version = \"v2\""
-    replaceWith='containerlog_schema_version = "v2"'
     echo "Downloading default ConfigMap"
     configMapYamlFile="$(curl -s https://raw.githubusercontent.com/microsoft/Docker-Provider/ci_prod/kubernetes/container-azm-ms-agentconfig.yaml)"
     echo "Setting containerlog_schema_version to v2"
-    echo "${configMapYamlFile/$replace/$replaceWith}" > container-azm-ms-agentconfig.yaml
+    configMapYamlFile=$(sed 's/#\[log_collection_settings.schema\]/[log_collection_settings.schema]/'<<<$configMapYamlFile)
+    configMapYamlFile=$(sed 's/# containerlog_schema_version = \"v2\"/containerlog_schema_version = "v2"/'<<<$configMapYamlFile)
+    echo "$configMapYamlFile" > container-azm-ms-agentconfig.yaml
     echo "Applying ConfigMap using kubectl apply"
     kubectl apply -f container-azm-ms-agentconfig.yaml
-    sleep 1m
 fi
