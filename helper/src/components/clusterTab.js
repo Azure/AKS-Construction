8e153865e39bea87f553fdcbf65da7b09058b834
oplaalvarez0001 <93328502+oplaalvarez0001@users.noreply.github.com>
2023-01-26 13:25:06 -0500

    Enable uptime SLA by default (#504)
    * Enable uptime SLA by default
    
    * Set Uptime SLA default based on Operational Principles
    
    * Updated API Server Non-AZ to 99.5% from 99.9

--- helper/src/components/clusterTab.js
+++ helper/src/components/clusterTab.js
@@ -70,7 +70,7 @@ export default function ({ tabValues, updateFn, featureFlag, invalidArray }) {
                             selectedKey={cluster.AksPaidSkuForSLA}
                             options={[
                                 { key: false, text: 'Free clusters with a service level objective (SLO) of 99.5%' },
-                                { key: true, text: 'Uptime SLA guarantees 99.95% availability of the Kubernetes API server endpoint for clusters that use Availability Zones' }
+                                { key: true, text: 'Uptime SLA guarantees 99.95% availability of the Kubernetes API server endpoint' }
                             ]}
                             onChange={(ev, { key }) => updateFn("AksPaidSkuForSLA", key)}
                         />
